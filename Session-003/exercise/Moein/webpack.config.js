3b1770959647cb2201a3651e741fb4513389b85b
Moein Alizadeh <truemoein@gmail.com>
2019-05-08 14:04:34 +0430

    Describe my exercise about WorkBox.

--- Session-003/exercise/Moein/webpack.config.js
+++ Session-003/exercise/Moein/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [{
---
+++ Session-004/exercises/Moein/README.md
@@ -0,0 +1,7 @@
+<div dir="rtl" style="direction:rtl;font-family:IRANSans,tahoma,arial,sans-serif">
+
+### مَن و وُرک‌باکس! 
+
+من یک تمرین در رابطه با وُرک‌باکس در وب‌سایت [WorkQube](https://WorkBox.ir/) انجام داده‌ام
+
+</div>
